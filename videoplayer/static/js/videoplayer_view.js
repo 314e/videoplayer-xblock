2b7e36009a19fe76a63f1e2ed88fbaf47a3a71ef
venkat-baddigam <venkat.baddigam@314ecorp.com>
2020-04-02 19:14:26 +0530

    debugging .

--- videoplayer/static/js/videoplayer_view.js
+++ videoplayer/static/js/videoplayer_view.js
@@ -6,7 +6,9 @@ function videoPlayerXBlockInitView(runtime, element) {
      * So here I make sure element is the jQuery object */
     if(element.innerHTML) element = $(element);
 
-    // var video = element.find('video:first');
+    var video = element.find('video:first');
+    console.log("video element is ", video)
+    console.log("runtime is ", runtime)
     // videojs(video.get(0), {playbackRates:[0.75,1,1.25,1.5,1.75,2]}, function() {});
     // debugger;
     // var url = document.querySelector("#videoPlayer").getAttribute("data-videourl");
@@ -20,11 +22,10 @@ function videoPlayerXBlockInitView(runtime, element) {
     initApp()
 }
 
-// var manifestUri =
-//     'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd';
+var manifestUri = 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd';
 
-var manifestUri =
-    "https://stream.314ecorp.com/314elearning/in/314e_sourcing_training/95.filetype_(4-47)-2/95.filetype_(4-47)-2.mpd";
+// var manifestUri =
+//     "https://stream.314ecorp.com/314elearning/in/314e_sourcing_training/95.filetype_(4-47)-2/95.filetype_(4-47)-2.mpd";
 
 function initApp() {
     console.log("from initApp.....")
